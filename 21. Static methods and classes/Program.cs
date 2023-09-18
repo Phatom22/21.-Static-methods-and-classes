c4359843cba811e444b3b207f64e29e7d34aa006
nomvula <nzshabangu95@gmail.com>
2023-09-18 11:43:33 +0200

    Add project files.

---
+++ 21. Static methods and classes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Static_methods_and_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+
+            Console.WriteLine(Math.Sqrt(144));
+         
+            SayHi.sayHello(" evans");
+
+            //freeze screen
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 21. Static methods and classes/SayHi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Static_methods_and_classes
+{
+    class SayHi
+    {
+        public static void sayHello(string name)
+        {
+
+            Console.WriteLine("hello" + name);
+        }
+    }
+}
